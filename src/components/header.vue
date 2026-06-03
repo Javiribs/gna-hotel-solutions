dddcd465ed00a4ff0d3c60598a9785e76c74e961
Javi Ribas <javiribs@gmail.com>
2026-06-03 12:39:00 +0200

    header update

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,32 @@
 <script setup>
-import logoFullWhite from '../assets/logos/logo-gnahs-white.png'
+import { ref, onMounted, onUnmounted } from 'vue'
+
+import logoWhite from '../assets/logos/logo-gnahs-white.png'
+import logoFullWhite from '../assets/logos/logo-gnahs-full-white.png'
+
+const isScrolled = ref(false)
+
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 40
+}
+
+onMounted(() => {
+  handleScroll()
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
-  <header class="header">
+  <header class="header" :class="{ 'header--scrolled': isScrolled }">
     <a href="#" class="header__brand">
-      <img :src="logoFullWhite" alt="GNA Hotel Solutions">
+      <img
+        :src="isScrolled ? logoFullWhite : logoWhite"
+        alt="GNA Hotel Solutions"
+      >
     </a>
 
     <nav class="header__nav">
